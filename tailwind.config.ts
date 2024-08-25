0c8a012810017d3ddfe5f8ecac93e99202f44682
Tohrusky <65994850+Tohrusky@users.noreply.github.com>
2024-08-25 20:01:19 +0800

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,9 @@
-import type { Config } from 'tailwindcss'
-// import defaultTheme from 'tailwindcss/defaultTheme'
+import type { Config } from 'tailwindcss';
 
-export default <Partial<Config>>{
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      // colors: {
-      //   primary: defaultTheme.colors.green
-      // },
-      transitionProperty: {
-        flow: 'background-position'
-      },
-      backgroundSize: {
-        flow: '400% auto'
-      },
-      backgroundImage: {
-        'nuxt-gradient':
-          'linear-gradient(30deg,#475569,#475569 25%,#0007dc 50%,#5400dc 75%,#dc00bf)'
-      }
-    }
-  }
-}
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
