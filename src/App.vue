f0ece4e68e56229d63c2c4e2e5eb47c1be9ddefe
Tohrusky <65994850+Tohrusky@users.noreply.github.com>
2024-08-30 23:43:13 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
 import { useOsTheme, darkTheme, lightTheme } from 'naive-ui'
-import { storeToRefs } from 'pinia'
-import { useSettingStore } from '@/store/setting'
 import { themeOverrides } from '@/theme'
+import Layout from '@/layout/Layout.vue'
+
 const osThemeRef = useOsTheme()
 const theme = computed(() => {
   return osThemeRef.value === 'dark' ? darkTheme : lightTheme
 })
-
-const { collapsed } = storeToRefs(useSettingStore())
 </script>
 
 <template>
@@ -17,35 +15,7 @@ const { collapsed } = storeToRefs(useSettingStore())
       <NDialogProvider>
         <NMessageProvider>
           <NNotificationProvider>
-            <NLayout has-sider position="absolute" :native-scrollbar="false">
-              <NLayoutSider
-                collapse-mode="width"
-                :collapsed-width="64"
-                :width="200"
-                :collapsed="collapsed"
-                show-trigger
-                :native-scrollbar="false"
-                bordered
-                @collapse="collapsed = true"
-                @expand="collapsed = false"
-              >
-                <Menu v-model:collapsed="collapsed" />
-              </NLayoutSider>
-              <NLayout :native-scrollbar="false">
-                <NLayoutHeader bordered class="p-[2vh] h-[6vh]">
-                  <Header />
-                </NLayoutHeader>
-                <NLayoutContent>
-                  <NLayout :native-scrollbar="false" class="p-[3vh] h-[94vh]">
-                    <RouterView v-slot="{ Component }">
-                      <KeepAlive>
-                        <component :is="Component" />
-                      </KeepAlive>
-                    </RouterView>
-                  </NLayout>
-                </NLayoutContent>
-              </NLayout>
-            </NLayout>
+            <Layout />
           </NNotificationProvider>
         </NMessageProvider>
       </NDialogProvider>
