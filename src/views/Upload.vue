0d623a5b48305774f775192c1058acf7979be151
Tohrusky <65994850+Tohrusky@users.noreply.github.com>
2024-08-31 21:40:42 +0800

    init

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,53 +1,41 @@
 <script setup lang="ts">
 import { NewTask } from '@/api'
 import { useNotification } from 'naive-ui'
+import type { NotificationType } from 'naive-ui'
 
 const notification = useNotification()
 
 const manualUploadKey = ref('')
 async function Upload(key: string) {
+  let notifyType: NotificationType = 'error'
+  let content: string = 'Upload failed'
+  let meta: string
+
   if (!key) {
-    console.error('Please input the key of the video')
-    notification['error']({
-      content: 'Upload failed',
-      meta: 'Please input the key of the video',
-      duration: 2500,
-      keepAliveOnHover: true,
-    })
-    return
+    meta = 'Please input the key of the video'
+  } else {
+    try {
+      const res = await NewTask({
+        video_key: key,
+      })
+      if (res.success) {
+        notifyType = 'success'
+        content = 'Upload success'
+        meta = 'Task has been initiated'
+      } else {
+        meta = res.error?.message || 'Unknown error'
+      }
+    } catch (error) {
+      meta = String(error) || 'Unknown error'
+    }
   }
 
-  try {
-    const res = await NewTask({
-      video_key: key,
-    })
-    console.log(res)
-    if (res.success) {
-      console.log('Upload success')
-      notification['success']({
-        content: 'Upload success',
-        meta: 'Task has been initiated',
-        duration: 2500,
-        keepAliveOnHover: true,
-      })
-    } else {
-      console.error('Upload failed: ' + res.error?.message)
-      notification['error']({
-        content: 'Upload failed',
-        meta: res.error?.message,
-        duration: 2500,
-        keepAliveOnHover: true,
-      })
-    }
-  } catch (error) {
-    console.error(error)
-    notification['error']({
-      content: 'Upload failed',
-      meta: String(error),
-      duration: 2500,
-      keepAliveOnHover: true,
-    })
-  }
+  notification[notifyType]({
+    content: content,
+    meta: meta,
+    duration: 2500,
+    keepAliveOnHover: true,
+  })
 }
 </script>
 
